8b5c73be7fc5549fbddbff629bea66f4af289d35
eyupGuc <meydanguresi@gmail.com>
2022-10-20 20:48:09 +0300

    m

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import AddTask from "../components/addTask/AddTask";
 import TaskList from "../components/taskList/TaskList";
+import Button from "react-bootstrap/Button";
 
 const Home = () => {
+  const[isOpen,setIsOpen]=useState(false);
+  const[text,setText]=useState("Show Task Bar")
+  const toggle=()=>{
+    setIsOpen(!isOpen);
+    const buttonText=isOpen ? "Show Task Bar" :"Hide Task Bar"
+    setText(buttonText)
+  }
   return (
     <div>
-      Home
-      <AddTask />
+      <Button size="lg" onClick={(e)=>toggle()} variant="danger">{text}</Button>
+
+      {isOpen && <AddTask />}
       <TaskList />
     </div>
   );

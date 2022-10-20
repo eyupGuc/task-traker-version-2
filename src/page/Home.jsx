2f2f6005c7c03bc16074c5df07066eebc519d3ce
eyupGuc <meydanguresi@gmail.com>
2022-10-20 21:09:25 +0300

    m

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,16 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddTask from "../components/addTask/AddTask";
 import TaskList from "../components/taskList/TaskList";
 import Button from "react-bootstrap/Button";
+import axios from "axios";
 
 const Home = () => {
   const[isOpen,setIsOpen]=useState(false);
   const[text,setText]=useState("Show Task Bar")
+  const[task,setTask]=useState()
+const url="https://6351820b3e9fa1244e608439.mockapi.io/apii/task"
+
   const toggle=()=>{
     setIsOpen(!isOpen);
     const buttonText=isOpen ? "Show Task Bar" :"Hide Task Bar"
     setText(buttonText)
   }
+
+  const getTask=async ()=>{
+    const {data}= await axios(url);
+    setTask(data)
+    console.log(data);
+    
+  }
+  useEffect(()=>{
+    getTask()
+  },[])
   return (
     <div>
       <Button size="lg" onClick={(e)=>toggle()} variant="danger">{text}</Button>
